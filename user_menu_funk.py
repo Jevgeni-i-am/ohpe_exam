9899d4d734f62cd39f29647858af7ca66ff9b5f9
Jevgeni <67290830+Jevgeni-i-am@users.noreply.github.com>
2022-12-16 16:12:43 +0200

    Commit 2
    Päivitetty leffaluettelon lataus.
    Työ ei ole läheskään valmis

--- user_menu_funk.py
+++ user_menu_funk.py
@@ -1,51 +1,52 @@
 import csv
 
 bookkaus = {}
-leffalista = []
+
 
 # Valinta 1.- Selaa kaikki näytökset
 
 
 def lataa_tiedot() -> list:
     """Lataa leffatiedot"""
-    # tyhjennetään ensin kaikki nykyiset tuotteet
-    global bookkaus
-    bookkaus.clear()
-
-    # Tiedoston nimi on aina sama
-    with open("naytokset.csv", "r") as tiedosto:
+    """Leffalistaa ladataan ulkopuoliselta resursilta, sen ei voi muokata"""
+    with open("elokuvat.csv", "r") as tiedosto:
         count = 0
+        list_to_dict = []
+        leffalista = {}
         reader = csv.reader(tiedosto)
         headers = next(reader)
-        if count == 0:
-            print('Headers: ', headers[0],
-                  headers[1], headers[-1], '# DELETE THIS LINE IN PROD !!!!')
-            count += 1
-        print("")
-        if count != 0:
-            for row in reader:
-                print(f'Position : {count}')
-                print(f'{headers[0]}     : {row[0]}')
-                print(f'{headers[1]}    : {row[1]}')
-                print(f'{headers[-1]}     : {row[-1]}')
-                print("")
-                count += 1
 
+        print("")
+
+        for row in reader:
+            print(f'Position : {count}')
+            print(f'{headers[0]}     : {row[0]}')
+            print(f'{headers[1]}    : {row[1]}')
+            print(f'{headers[-1]}     : {row[-1]}')
+            print("")
+
+            list_to_dict = (f'{row[0]}, {row[1]}, {row[-1]}')
+            list_to_dict
+            leffalista = dict({count: list_to_dict})
+
+            count += 1
+        with open('out.txt', 'w') as tiedosto:
+            tiedosto.writelines(f'{row}\n' for row in leffalista)
+
+        print("LEFFALISTA::::", leffalista)
+        print("Valitse elokuva posiition perusteella:")
+        pos = input("Positio: ")
+
+        """Kirjoitetaan leffalistaa tiedostoon ja kaikki toimeenpiteet tehdään vain sen listan kanssa """
+        """Haetaan posiition perusteella näytös"""
+
+        with open("bookkaus.txt", "a") as tiedosto:
+            for nimi, maara in bookkaus.items():
+                rivi = f"{nimi},{maara}\n"
+                tiedosto.write(rivi)
 
 
 # Valinta 6.- Tallenna valintasi
-
-
-def tallenna_tiedot():
-    """Tallentaa varaustiedot tiedostoon"""
-    # Tiedoston nimi on aina sama
-    global bookkaus
-    with open("bookkaus.csv", "w") as tiedosto:
-        for nimi, maara in bookkaus.items():
-            rivi = f"{nimi},{maara}\n"
-            tiedosto.write(rivi)
-
-
 #
 #
 #
